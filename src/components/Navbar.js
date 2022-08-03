2a98cac900aaa7edad51d4f1b5c95405c01e825e
Ilies <benosman.ilies@hetic.net>
2022-08-03 23:05:45 +0100

    fix scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,107 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
-export default function Navbar() {
-    return (
-        <header>
-            <nav className="container">
-                <a href="#" className="logo">Cyl<span>exx</span></a>
-                <div class="link">
-                    <ul>
-                        <li>
-                            <a href="#" className="nav-link">Acceuil</a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link">Article</a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link">A propos </a>
-                        </li>
-                        <li>
-                            <a href="#" className="nav-link">Contact</a>
-                        </li>
-                    </ul>
-                    <i class="uil uil-moon toggle-btn"></i>
-                </div>
-            </nav>
-        </header>
-    );
-}
-
-function stickyNavbar() {
+export const Navbar = () => {
+  const stickyNavbar = () => {
     const header = document.querySelector("header");
-    console.log('header', header)
     header.classList.toggle("scrolled", window.pageYOffset > 0);
-}
-stickyNavbar();
-window.addEventListener("scroll", stickyNavbar);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickyNavbar);
+  }, []);
+
+  return (
+    <header>
+      <Nav>
+        <a href="#" className="logo">
+          Cyl<span>exx</span>
+        </a>
+        <LinkContainer>
+          <LinkUl>
+            <li>
+              <Link href="#">
+                Acceuil
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                Article
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                A propos{" "}
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                Contact
+              </Link>
+            </li>
+          </LinkUl>
+          <i className="uil uil-moon toggle-btn"></i>
+        </LinkContainer>
+      </Nav>
+    </header>
+  );
+};
+
+const Nav = styled("nav")`
+  position: relative;
+  width: 100%;
+  max-width: 75rem;
+  padding: 0 1.5rem;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const LinkContainer = styled("div")`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const LinkUl = styled("ul")`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const Link = styled("a")`
+    position: relative;
+    display: inline-block;
+    margin: 0 1rem;
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    letter-spacing: 0.5px;
+    color: var(--text-color);
+    font-weight: 500;
+    opacity: 0.9;
+    transition: 0.3s;
+
+  ::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 10px;
+    border-radius: 5px;
+    background-color: var(--main-color-1);
+    bottom: 0;
+    left: -5px;
+    opacity: 0;
+    transition: 0.3s;
+  }
+
+  :hover {
+    color: var(--heading-color);
+  }
+
+  :hover::after {
+    width: 100%;
+    opacity: 0.2;
+  }
+`;
